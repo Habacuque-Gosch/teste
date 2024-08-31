c76a75a13b313417fa6794450dcf70ae2cb9f429
habacuque-gosch <habacuke14@gmail.com>
2024-08-31 18:32:41 -0300

    add new router

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
 import uvicorn
-import modules
-import modules
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
 import modules.routers.route1_router
 
 
@@ -9,13 +9,30 @@ import modules.routers.route1_router
 
 app = FastAPI()
 
-@app.get("/")
-def hello_word() -> str:
-    return "Hello Word"
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
 
 app.include_router(modules.routers.route1_router.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
--- modules/routers/route1_router.py
+++ modules/routers/route1_router.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 
 router = APIRouter(prefix="/router1-router")
 
 
+class ContaPagarResponse(BaseModel):
+    pass
+
+
+
+
 @router.get("/")
 def router1_test():
+        
     return [
         {"contas1": "contas123"},
         {"contas1": "contas123"},
