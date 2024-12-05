01e987256a7b13430b8b9723ebc9f186a662a4bc
habacuque-gosch <habacuke14@gmail.com>
2024-12-05 06:41:00 -0300

    create class Item and Category

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 import modules.routers.route1_router
+from enum import Enum
 
 
 
@@ -10,14 +11,25 @@ import modules.routers.route1_router
 app = FastAPI()
 
 
+class Category(Enum):
+    tools = 'tools'
+    consumables = 'consumables'
+    pass
+
 class Item(BaseModel):
     name: str
     price: float
+    count: int
+    id: int
+    category: Category
 
+    def __str__(self):
+        return self.name
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
+items = {
+    0: Item(name='Hammer', price=9.99, count=20, id=0, category=Category.tools),
+    1: Item(name='Nails', price=1.99, count=4, id=1, category=Category.consumables)
+}
 
 
 @app.get("/itens/{item_id}")
@@ -29,7 +41,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
-app.include_router(modules.routers.route1_router.router)
+# app.include_router(modules.routers.route1_router.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
